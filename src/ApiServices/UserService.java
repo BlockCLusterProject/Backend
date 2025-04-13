04ee0981b8121cb29c5fa3b8939c8e6e65e8ee44
thekarvanak <magita560@gmail.com>
2025-04-13 11:24:16 -0500

    UPDATE & AND endpoints para lectura de QR

--- src/ApiServices/UserService.java
+++ src/ApiServices/UserService.java
@@ -5,13 +5,25 @@ import Models.Client;
 import Models.Movie;
 import Repository.OwnRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.util.List;
 
 @Service
 public class UserService {
 	private final OwnRepository repository;
+	private final RestTemplate restTemplate = new RestTemplate();
 
 	@Autowired
 	public UserService(OwnRepository repository) throws InterruptedException {
@@ -33,6 +45,23 @@ public class UserService {
 	
 	public boolean registerClient (Client user) {
 		return repository.registerClient(user);
+	}
+	
+	public byte[] generateQr(String message) {
+		final int size = 300;
+		final String format = "png";
+			String apiUrl;
+			URL url;
+			/*apiUrl = String.format(
+					"https://api.qrserver.com/v1/create-qr-code/?data=%s&size=%dx%d",
+					URLEncoder.encode(message, "UTF-8"), size, size);*/
+			apiUrl = String.format(
+					"https://api.qrserver.com/v1/create-qr-code/?data=%s&size=%dx%d",
+					message, size, size);
+			ResponseEntity<byte[]> response = restTemplate.getForEntity(apiUrl, byte[].class);
+			System.out.println(response.getBody());
+			return response.getBody();
+			
 	}
 
 }
