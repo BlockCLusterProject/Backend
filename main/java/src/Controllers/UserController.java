281bb70a1f3eedfadac6a01385d3824df657051b
Andrea Giraldo <andreagiraldo2391@gmail.com>
2025-05-24 18:06:38 -0500

    Se añade validacion de jwt

--- main/java/src/Controllers/UserController.java
+++ main/java/src/Controllers/UserController.java
@@ -4,6 +4,7 @@
  */
 package Controllers;
 
+import ApiServices.JwtService;
 import ApiServices.UserService;
 import Entities.PurchaseHistory;
 import Models.Person;
@@ -20,6 +21,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -41,10 +43,13 @@ import java.util.List;
 @Tag(name = "User", description = "API para la gesti�n de usuarios")
 public class UserController {
     private final UserService userService;
+    private final JwtService jwtService;
 
     @Autowired
-    public UserController(UserService userService) {
+    public UserController(UserService userService, JwtService jwtService) {
         this.userService = userService;
+        this.jwtService = jwtService;
+        
     }
     
     @PostMapping("/movie/add-purchase-history")
@@ -138,9 +143,15 @@ public class UserController {
     		@ApiResponse(responseCode = "500", description = "Error interno del servidor")
     })
     
-    public ResponseEntity<Person> validateUser(
+    public ResponseEntity<?> validateUser(
             @RequestParam(required = false) String user,
-            @RequestParam(required = false) String password) {
+            @RequestParam(required = false) String password,
+    		@RequestHeader(value = "Authorization",required = true) String token){
+    	
+    	String userToken = jwtService.extractToken(token);
+        if (token == null || !jwtService.validateJwtToken(userToken)) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Token JWT inválido o ausente.");
+        }
     	if (user == null || password == null) {
     		return new ResponseEntity<>(null, HttpStatus.NOT_FOUND); 
     	}
